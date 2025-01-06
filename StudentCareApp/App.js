3d13183c925a3462000df4d729db97df5aa1223e
SanduniAmasha <139688263+SanduniAmasha@users.noreply.github.com>
2025-01-06 17:52:49 +0000

    final touch

--- StudentCareApp/App.js
+++ StudentCareApp/App.js
@@ -1,4 +1,3 @@
-import React, {useSate} from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StyleSheet } from "react-native";
@@ -6,28 +5,36 @@ import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import Login from "./components/Login";
-
-const Stack = createNativeStackNavigator();
+import Home from "./components/Home";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <SafeAreaProvider>
       <PaperProvider>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="login"
-           screenOptions={{
-            eaderStyle: {
-              backgroundColor: "#4b0150",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              color: "#fff",
-            },
-            headerTitleAlign: "center",
-          }}>
+          <Stack.Navigator
+            initialRouteName="login"
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: "#4b0150",
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                color: "#fff",
+              },
+              headerTitleAlign: "center",
+            }}
+          >
             <Stack.Screen
               name="login"
               component={Login}
+              options={{ title: "UoV Student Care" }}
+            />
+            <Stack.Screen
+              name="home"
+              component={Home}
               options={{ title: "UoV Student Care" }}
             />
           </Stack.Navigator>
@@ -40,8 +47,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
